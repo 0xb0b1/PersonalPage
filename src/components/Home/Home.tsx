96c4f8b679faed358cf5849eb206512b14b46a95
0xb0b1 <vcente82@gmail.com>
2020-12-19 12:56:41 -0300

    work in progress

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
 
-import { HomeContainer } from '../Nav/styles';
+import { HomeContainer } from './styles';
 
 // images
 import man from './../images/man.svg';
---
+++ src/components/Home/styles.ts
@@ -0,0 +1,6 @@
+import styled, { StyledComponent } from 'styled-components';
+
+export const HomeContainer = styled.div`
+  margin: 0;
+  padding: 0;
+`
