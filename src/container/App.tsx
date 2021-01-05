b283f370624227571e088a2226d635bac5ad5637
0xb0b1 <vcente82@gmail.com>
2021-01-05 12:31:40 -0300

    ...

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,9 +1,4 @@
 import React from 'react'
-import Header from '../components/Header/Header'
-import Home from '../components/Home/Home'
-import Skills from '../components/Skills/Skills'
-import Contact from '../components/Contact/Contact'
-import Footer from '../components/Footer/Footer'
 import MainContent from './style'
 
 const App: React.FC = () => {
